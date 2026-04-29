d9d9ec5c2df81e8334ce1421fe0bae5895163463
pindix <pindixy@gmail.com>
2026-04-29 13:48:28 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'mpindi-tecmed-v1';
 const urlsToCache = [
-    '/cp app/',
-    '/cp app/index.html',
-    '/cp app/style.css',
-    '/cp app/script.js',
-    '/cp app/dosagem/index.html',
-    '/cp app/exames/index.html'
+    '/cpApp/',
+    '/cpApp/index.html',
+    '/cpApp/style.css',
+    '/cpApp/script.js',
+    '/cpApp/dosagem/index.html',
+    '/cpApp/exames/index.html'
 ];
 
 self.addEventListener('install', (event) => {
